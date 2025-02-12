c00a1e5da1f256d021300752f5b46d5f9db8affc
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-02-12 14:47:57 +0900

    #3.13 Redirect & Response.json in loader function

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -6,7 +6,7 @@ import { PostCard } from "~/features/community/components/post-card";
 import { IdeaCard } from "~/features/ideas/components/idea-card";
 import { JobCard } from "~/features/jobs/components/job-card";
 import { TeamCard } from "~/features/teams/components/team-card";
-import type { Route } from "~/+types/root";
+
 export const meta: MetaFunction = () => {
     return [
         { title: "Home | wemake" },
@@ -14,13 +14,9 @@ export const meta: MetaFunction = () => {
     ]
 }
 
-export const loader = async () => {
-    return {
-        hello: "world"
-    }
-}
 
-export default function HomePage({ loaderData }: Route.ComponentProps<typeof loader>) {
+
+export default function HomePage() {
     return (
         <div className="px-20 space-y-40">
             <div className="grid grid-cols-3 gap-4">
@@ -28,7 +24,7 @@ export default function HomePage({ loaderData }: Route.ComponentProps<typeof loa
                     <h2 className="text-2xl font-bold leading-tight tracking-tight">Today's Products</h2>
                     <p className="text-sm text-muted-foreground">the best products made by our community today.</p>
                     <Button variant="link" asChild className="text-lg p-0">
-                        <Link to="/products/leaderboard">Explore all products &rarr;</Link>
+                        <Link to="/products/leaderboards">Explore all products &rarr;</Link>
                     </Button>
                 </div>
 
