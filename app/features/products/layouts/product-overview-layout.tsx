9009ff9bd352ad84121d319fef8a894831aa0fc0
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-02-26 14:05:12 +0900

    #3.26 Layout Routes && NavLink

---
+++ app/features/products/layouts/product-overview-layout.tsx
@@ -0,0 +1,48 @@
+import { ChevronUpIcon, StarIcon } from "lucide-react";
+import { NavLink, Outlet } from "react-router";
+import { Button, buttonVariants } from "~/common/components/ui/button";
+import { cn } from "~/lib/utils";
+
+export default function ProductOverviewLayout() {
+    return (
+        <div className="space-y-10">
+            <div className="flex justify-between">
+                <div className="flex gap-10">
+                    <div className="size-40 rounded-xl shadow-xl bg-primary/50">
+                    </div>
+                    <div>
+                        <h1 className="text-5xl font-bold">Product Name</h1>
+                        <p>Product ID: 1</p>
+                        <p className="text-2xl font-light text-muted-foreground">Product Description</p>
+                        <div className="mt-5 flex items-center gap-5">
+                            <div className="flex gap-2">
+                                {Array.from({ length: 5 }).map((_, index) => (
+                                    <StarIcon key={index} className="w-4 h-4 text-yellow-500" fill="currentColor" />
+                                ))}
+                            </div>
+                            <span className="text-muted-foreground">100 reviews</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex gap-4">
+                    <Button variant="outline" size="lg" className="text-lg h-14 px-10">
+                        Visit Website
+                    </Button>
+                    <Button variant="outline" size="lg" className="text-lg h-14 px-10">
+                        <ChevronUpIcon className="w-4 h-4" />
+                        Upvote (100)
+                    </Button>
+                </div>
+            </div>
+            <div className="flex gap-2">
+                <Button variant="outline" >
+                    <NavLink to={`/products/1/overview`} className={({ isActive }) => isActive ? cn(buttonVariants({ variant: "ghost" }), "text-pink-500 font-bold") : ""}>Overview</NavLink>
+                </Button>
+                <Button variant="outline" >
+                    <NavLink to={`/products/1/reviews`} className={({ isActive }) => isActive ? cn(buttonVariants({ variant: "ghost" }), "text-pink-500 font-bold") : ""}>Reviews</NavLink>
+                </Button>
+            </div>
+            <Outlet />
+        </div>
+    );
+}
--- app/features/products/pages/product-overview-page.tsx
+++ app/features/products/pages/product-overview-page.tsx
@@ -32,42 +32,6 @@ export default function ProductOverviewPage({ loaderData }: Route.ComponentProps
 
     return (
         <div className="space-y-4">
-            <div className="flex justify-between">
-                <div className="flex gap-10">
-                    <div className="size-40 rounded-xl shadow-xl bg-primary/50">
-                    </div>
-                    <div>
-                        <h1 className="text-5xl font-bold">Product Name</h1>
-                        <p>Product ID: {productId}</p>
-                        <p className="text-2xl font-light text-muted-foreground">Product Description</p>
-                        <div className="mt-5 flex items-center gap-5">
-                            <div className="flex gap-2">
-                                {Array.from({ length: 5 }).map((_, index) => (
-                                    <StarIcon key={index} className="w-4 h-4 text-yellow-500" fill="currentColor" />
-                                ))}
-                            </div>
-                            <span className="text-muted-foreground">100 reviews</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex gap-4">
-                    <Button variant="outline" size="lg" className="text-lg h-14 px-10">
-                        Visit Website
-                    </Button>
-                    <Button variant="outline" size="lg" className="text-lg h-14 px-10">
-                        <ChevronUpIcon className="w-4 h-4" />
-                        Upvote (100)
-                    </Button>
-                </div>
-            </div>
-            <div className="flex gap-2">
-                <Button variant="outline" asChild>
-                    <Link to={`/products/${productId}/overview`}>Overview</Link>
-                </Button>
-                <Button variant="outline" asChild>
-                    <Link to={`/products/${productId}/reviews`}>Reviews</Link>
-                </Button>
-            </div>
             <div className="space-y-10">
                 <div>
                     <h3 className="text-2xl font-semibold mb-4">What is this product?</h3>
