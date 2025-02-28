75e8771fad19b614a5e8c55f43e47aa66c54ae77
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2025-02-28 13:16:23 +0900

    #3.27 Product Review

---
+++ app/features/products/components/review-card.tsx
@@ -0,0 +1,44 @@
+import { Avatar, AvatarFallback, AvatarImage } from "~/common/components/ui/avatar"
+import { StarIcon } from "lucide-react"
+
+interface ReviewCardProps {
+    avatarUrl: string
+    avatarFallback: string
+    name: string
+    username: string
+    rating: number
+    content: string
+    createdAt: string
+}
+
+export function ReviewCard({
+    avatarUrl,
+    avatarFallback,
+    name,
+    username,
+    rating,
+    content,
+    createdAt
+}: ReviewCardProps) {
+    return (
+        <div className="space-y-5">
+            <div className="flex items-center gap-2">
+                <Avatar>
+                    <AvatarImage src={avatarUrl} />
+                    <AvatarFallback>{avatarFallback}</AvatarFallback>
+                </Avatar>
+                <div>
+                    <h4 className="text-lg font-semibold">{name}</h4>
+                    <p className="text-sm text-muted-foreground">@{username}</p>
+                </div>
+            </div>
+            <div className="flex gap-2">
+                {Array.from({ length: rating }).map((_, index) => (
+                    <StarIcon key={index} className="w-4 h-4 text-yellow-500" fill="currentColor" />
+                ))}
+            </div>
+            <p className="text-sm text-muted-foreground">{content}</p>
+            <span className="text-sm text-muted-foreground">{createdAt}</span>
+        </div>
+    )
+} 
--- app/features/products/pages/product-reviews-page.tsx
+++ app/features/products/pages/product-reviews-page.tsx
@@ -1,7 +1,37 @@
+import { Button } from "~/common/components/ui/button"
+import { ReviewCard } from "../components/review-card"
+
+export function meta() {
+    return [
+        { title: "Product Reviews" },
+        { name: "description", content: "Product Reviews" }
+    ]
+}
+
 export default function ProductReviewsPage() {
     return (
-        <div>
-            <h1>Product Reviews</h1>
+        <div className="space-y-10 max-w-screen-md">
+            <div className="flex justify-between items-center">
+                <h2 className="text-2xl font-semibold">10 reivews</h2>
+                <Button variant="outline">Write a review</Button>
+            </div>
+            <div className="space-y-20">
+                {Array.from({ length: 10 }).map((_, index) => (
+                    <ReviewCard
+                        key={index}
+                        avatarUrl="https://github.com/azerckid.png"
+                        avatarFallback="CN"
+                        name="John Doe"
+                        username="username"
+                        rating={5}
+                        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam, quos. 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+                        createdAt="10 days ago"
+                    />
+                ))}
+            </div>
         </div>
     )
 }
