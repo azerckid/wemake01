8ff647b9e7de215ec0cb121d5486c3d8612166ac
azerckid <azerckid@gmail.com>
2025-04-16 14:36:45 +0900

    #8.4 Post replies part two

--- app/features/community/components/reply.tsx
+++ app/features/community/components/reply.tsx
@@ -1,4 +1,4 @@
-import { Form, Link } from "react-router";
+import { Form, Link, useOutletContext } from "react-router";
 import { DotIcon, MessageCircleIcon } from "lucide-react";
 import { Button } from "~/common/components/ui/button";
 import {
@@ -11,11 +11,13 @@ import { Textarea } from "~/common/components/ui/textarea";
 import { DateTime } from "luxon";
 
 interface ReplyProps {
+    name: string;
     username: string;
     avatarUrl: string | null;
     content: string;
     timestamp: string | null;
     topLevel: boolean;
+    topLevelId?: number;
     replies?: {
         post_reply_id: number;
         reply: string;
@@ -29,26 +31,44 @@ interface ReplyProps {
 }
 
 export function Reply({
+    name,
     username,
     avatarUrl,
     content,
     timestamp,
     topLevel,
     replies,
+    topLevelId,
 }: ReplyProps) {
     const [replying, setReplying] = useState(false);
     const toggleReplying = () => setReplying((prev) => !prev);
+    const {
+        isLoggedIn,
+        name: loggedInName,
+        avatar,
+    } = useOutletContext<{
+        isLoggedIn: boolean;
+        name: string;
+        avatar: string;
+    }>();
+
+    const handleSubmit = () => {
+        setTimeout(() => {
+            setReplying(false);
+        }, 100);
+    };
+
     return (
         <div className="flex flex-col gap-2 w-full">
             <div className="flex items-start gap-5 w-2/3">
                 <Avatar className="size-14">
-                    <AvatarFallback>{username[0]}</AvatarFallback>
+                    <AvatarFallback>{name[0]}</AvatarFallback>
                     {avatarUrl ? <AvatarImage src={avatarUrl} /> : null}
                 </Avatar>
                 <div className="flex flex-col gap-2 items-start w-full">
                     <div className="flex gap-2 items-center">
                         <Link to={`/users/${username}`}>
-                            <h4 className="font-medium">{username}</h4>
+                            <h4 className="font-medium">{name}</h4>
                         </Link>
                         <DotIcon className="size-5" />
                         <span className="text-xs text-muted-foreground">
@@ -56,22 +76,32 @@ export function Reply({
                         </span>
                     </div>
                     <p className="text-muted-foreground">{content}</p>
-                    <Button variant="ghost" className="self-end" onClick={toggleReplying}>
-                        <MessageCircleIcon className="size-4" />
-                        Reply
-                    </Button>
+                    {isLoggedIn ? (
+                        <Button
+                            variant="ghost"
+                            className="self-end"
+                            onClick={toggleReplying}
+                        >
+                            <MessageCircleIcon className="size-4" />
+                            Reply
+                        </Button>
+                    ) : null}
                 </div>
             </div>
             {replying && (
-                <Form className="flex items-start gap-5 w-3/4">
+                <Form className="flex items-start gap-5 w-3/4" method="post" onSubmit={handleSubmit}>
+                    <input type="hidden" name="topLevelId" value={topLevelId} />
                     <Avatar className="size-14">
-                        <AvatarFallback>N</AvatarFallback>
-                        <AvatarImage src="https://github.com/zizimoos.png" />
+                        <AvatarFallback>{loggedInName[0]}</AvatarFallback>
+                        <AvatarImage src={avatar} />
                     </Avatar>
                     <div className="flex flex-col gap-5 items-end w-full">
                         <Textarea
+                            name="reply"
+                            autoFocus
                             placeholder="Write a reply"
                             className="w-full resize-none"
+                            defaultValue={`@${username} `}
                             rows={5}
                         />
                         <Button>Reply</Button>
@@ -82,11 +112,14 @@ export function Reply({
                 <div className="pl-20 w-full">
                     {replies.map((reply) => (
                         <Reply
+                            key={reply.post_reply_id}
+                            name={reply.user.name}
                             username={reply.user.username}
                             avatarUrl={reply.user.avatar_url}
                             content={reply.reply}
                             timestamp={reply.created_at}
                             topLevel={false}
+                            topLevelId={reply.post_reply_id}
                         />
                     ))}
                 </div>
